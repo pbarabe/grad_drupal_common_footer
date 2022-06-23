f0f98ac901f7584ba516ec3c8e9aa7ba0cf0cee5
Patrick Barabe <pbarabe@arizona.edu>
2022-06-23 13:02:59 -0700

    Feature: add a search form [WIP]
    Adds the (beginnings of) a search form to the common footer
    that can be used to trigger a search of our global CSE for
    the grad.arizona.edu.
    
    With this commit, the form markup is stripped by drupal
    before the footer pages is rendered.
    
    This commit also makes the footer links prettier with some
    updates to the AZ Boostrap CSS classes and consolidating
    the links into 2 columns
    
    references #1

--- src/Plugin/Block/GradDrupalCommonFooterBlock.php
+++ src/Plugin/Block/GradDrupalCommonFooterBlock.php
@@ -8,7 +8,7 @@ use Drupal\Core\Block\BlockBase;
  * Provides a Grad Footer Block with common links
  *
  * @Block(
- *   id = "grad_common_block",
+ *   id = "grad_common_footer_block",
  *   admin_label = @Translation("Grad Common Footer")
  * )
  */
@@ -24,12 +24,15 @@ class GradDrupalCommonFooterBlock extends BlockBase {
   <div class="col-12 col-sm-12 col-md-4 col-lg-4 pb-4 field__item">
     <div class="card card-borderless bg-transparent h-100">
       <div class="card-body d-flex flex-column">
-        <div class="card-text"><p class="text-align-center"><a href="https://grad.arizona.edu/catalog">Graduate Catalog</a><br>
-            <a href="https://grad.arizona.edu/admissions">Graduate Admissions</a><br>
-            <a href="https://grad.arizona.edu/gsas">Graduate Student Academic Services</a><br>
-            <a href="https://grad.arizona.edu/funding">Funding Resources</a></p>
+        <div class="card-text">
+          <p class="text-align-center">
+            <a class="font-weight-bold small text-decoration-none" href="https://grad.arizona.edu/catalog">Graduate Catalog</a><br>
+            <a class="font-weight-bold small text-decoration-none" href="https://grad.arizona.edu/admissions">Graduate Admissions</a><br>
+            <a class="font-weight-bold small text-decoration-none" href="https://grad.arizona.edu/gsas">Graduate Student Academic Services</a><br>
+            <a class="font-weight-bold small text-decoration-none" href="https://grad.arizona.edu/funding">Funding Resources</a><br>
+            <a class="font-weight-bold small text-decoration-none" href="https://grad.arizona.edu/policies">Policies</a>
+          </p>
         </div>
-        <div class="mt-auto"></div>
       </div>
     </div>
   </div>
@@ -37,11 +40,15 @@ class GradDrupalCommonFooterBlock extends BlockBase {
   <div class="col-12 col-sm-12 col-md-4 col-lg-4 pb-4 field__item">
     <div class="card card-borderless bg-transparent h-100">
       <div class="card-body d-flex flex-column">
-        <div class="card-text"><p class="text-align-center"><a href="https://gradcenter.arizona.edu/">The Graduate Center</a><br>
-            <a href="https://apply.grad.arizona.edu/">GradApp (Future Students)</a><br>
-            <a href="https://grad.arizona.edu/gradpath">GradPath (Current Students)</a></p>
+        <div class="card-text">
+          <p class="text-align-center">
+            <a class="font-weight-bold small text-decoration-none" href="https://gradcenter.arizona.edu/">The Graduate Center</a><br>
+            <a class="font-weight-bold small text-decoration-none" href="https://apply.grad.arizona.edu/">GradApp (Future Students)</a><br>
+            <a class="font-weight-bold small text-decoration-none" href="https://grad.arizona.edu/gradpath">GradPath (Current Students)</a><br>
+            <a class="font-weight-bold small text-decoration-none" href="https://grad.arizona.edu/equity">Commitment to Equity</a><br>
+            <a class="font-weight-bold small text-decoration-none" href="https://grad.arizona.edu/tools/directory">Contact Us</a>
+          </p>
         </div>
-        <div class="mt-auto"></div>
       </div>
     </div>
   </div>
@@ -49,11 +56,19 @@ class GradDrupalCommonFooterBlock extends BlockBase {
   <div class="col-12 col-sm-12 col-md-4 col-lg-4 pb-4 field__item">
     <div class="card card-borderless bg-transparent h-100">
       <div class="card-body d-flex flex-column">
-        <div class="card-text"><p class="text-align-center"><a href="https://grad.arizona.edu/policies">Policies</a><br>
-            <a href="https://grad.arizona.edu/equity">Commitment to Equity</a><br>
-            <a href="https://grad.arizona.edu/tools/directory">Contact Us</a></p>
+        <div class="card-text">
+          <p class="text-align-center">
+            HELLO WORLD (remove me)
+          </p>
+          <div>
+            <form class="form-inline" id="search-form" method="GET" action="https://grad.arizona.edu/tools/search">
+              <input name="q" type="search" aria-label="Search" placeholder="Search" />
+              <span class="form-actions form-wrapper">
+                <input type="submit" class="form-submit" />
+              </span>
+            </form>
+          </div>
         </div>
-        <div class="mt-auto"></div>
       </div>
     </div>
   </div>
